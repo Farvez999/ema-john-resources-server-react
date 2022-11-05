d3bae858109c0b5342a06fa7afdcce065493a98b
Farvez999 <engrfrzabdullah999@gmail.com>
2022-11-05 17:04:23 +0600

    pagination api  simple ema-john server

--- index.js
+++ index.js
@@ -12,8 +12,36 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mordayw.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const productCollection = client.db("emaJohn").collection("products");
+
+        app.get('/products', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.skip(page * size).limit(size).toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count, products });
+        })
+
+        app.post('/productsByIds', async (req, res) => {
+            const ids = req.body;
+            const objectIds = ids.map(id => ObjectId(id))
+            const queryA = { _id: { $in: objectIds } };
+            const cursor = productCollection.find(queryA);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
